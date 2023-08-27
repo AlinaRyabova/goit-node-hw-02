108da6b3e55d812f5c632b6856fa42128235ae94
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-08-27 18:29:05 +0300

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const jimp = require("jimp");
 
 const { User } = require("../models/user");
 
@@ -81,6 +82,8 @@ const updateAvatar = async (req, res) => {
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
+  const image = await jimp.read(resultUpload);
+  await image.cover(250, 250).write(resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
 
